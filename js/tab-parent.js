49c5a2e193f481780fac88ee6a78e992a2ad82b5
ortiza5 <43008712+ortiza5@users.noreply.github.com>
2020-06-11 15:52:05 -0600

    increased similarity to extension version and fixed recent bug

--- js/tab-parent.js
+++ js/tab-parent.js
@@ -12,62 +12,85 @@
       enabled: false,
     });
 
-    // listener for clicks on context menu item
-    chrome.contextMenus.onClicked.addListener(function (info) {
-      //* BUG-FIX: multiple windows led to creation of duplicate tabs equal to number of windows
-      let doesntHaveFocus = document.querySelector("#browser.isblurred");
-      if (doesntHaveFocus) return;
+    // Determines if the parent tab is still valid and switches to it or creates a new tab with the valid url
+    function goToParent() {
+      // get the active tab
+      chrome.tabs.query({ currentWindow: true, active: true }, (tab) => {
+        tab = tab[0];
+        let parentTabId = tab.openerTabId;
+        let tabId = tab.id;
 
-      // Match id used in context menu item creation
-      if (info.menuItemId === "parent-tab") {
-        // get the active tab
-        chrome.tabs.query({ currentWindow: true, active: true }, (tab) => {
-          tab = tab[0];
-          let parentTabId = tab.openerTabId;
-          let tabId = tab.id;
+        // see what the tab's parent tab url is supposed to be
+        chrome.storage.local.get(tabId.toString(), (storageItems) => {
+          let savedUrl = storageItems[tabId.toString()];
 
-          // see what the tab's parent tab url is supposed to be
-          chrome.storage.local.get(tabId.toString(), (storageItems) => {
-            let savedUrl = storageItems[tabId.toString()];
+          // if the parent tab still exists ...
+          if (parentTabId) {
+            // ... check the current url of the parent tab
+            chrome.tabs.get(parentTabId, (parentTab) => {
+              let parentTabUrl = parentTab.url ? parentTab.url : parentTab.pendingUrl ? parentTab.pendingUrl : null;
 
-            // if the parent tab still exists ...
-            if (parentTabId) {
-              // ... check the current url of the parent tab
-              chrome.tabs.get(parentTabId, (parentTab) => {
-                let parentTabUrl = parentTab.url ? parentTab.url : parentTab.pendingUrl ? parentTab.pendingUrl : null;
-
-                if (parentTabUrl === savedUrl) {
-                  // if the parent tab has the same url as it did in the past, switch to the parent tab
-                  chrome.tabs.update(parentTabId, { highlighted: true });
-                } else if (savedUrl && savedUrl !== "") {
-                  // create a new tab since the parent tab no longer has the correct url
-                  chrome.tabs.create({ url: savedUrl });
-                }
-              });
-            } else if (savedUrl && savedUrl !== "") {
-              // the parent tab no longer exists, so create a new tab with the saved url
-              chrome.tabs.create({ url: savedUrl });
-            }
-          });
+              if (parentTabUrl === savedUrl) {
+                // if the parent tab has the same url as it did in the past, switch to the parent tab
+                chrome.tabs.update(parentTabId, { highlighted: true });
+              } else if (savedUrl && savedUrl !== "") {
+                // create a new tab since the parent tab no longer has the correct url
+                chrome.tabs.create({ url: savedUrl });
+              }
+            });
+          } else if (savedUrl && savedUrl !== "") {
+            // the parent tab no longer exists, so create a new tab with the saved url
+            chrome.tabs.create({ url: savedUrl });
+          }
         });
-      }
-    });
+      });
+    }
 
-    // whenever a new tab is created, add its parent tab's url to storage
-    chrome.tabs.onCreated.addListener((tab) => {
+    // save a given tab's parent url to local storage
+    function saveParentUrl(tab) {
       let tabId = tab.id;
       let storageOBJ = {};
       if (tab.openerTabId) {
         chrome.tabs.get(tab.openerTabId, (parentTab) => {
           let url = parentTab.url ? parentTab.url : parentTab.pendingUrl ? parentTab.pendingUrl : "";
           storageOBJ[tabId.toString()] = url;
-          chrome.storage.local.set(storageOBJ);
+          chrome.storage.local.set(storageOBJ, function () {
+            enableDisableContextMenu(tabId);
+          });
         });
       } else {
         storageOBJ[tabId.toString()] = "";
-        chrome.storage.local.set(storageOBJ, function () {});
+        chrome.storage.local.set(storageOBJ, function () {
+          enableDisableContextMenu(tabId);
+        });
+      }
+    }
+
+    function enableDisableContextMenu(tabId) {
+      chrome.storage.local.get(tabId.toString(), (storageItems) => {
+        let savedUrl = storageItems[tabId.toString()];
+        if (!savedUrl || savedUrl === "") {
+          chrome.contextMenus.update("parent-tab", { enabled: false });
+        } else {
+          chrome.contextMenus.update("parent-tab", { enabled: true });
+        }
+      });
+    }
+
+    // listener for clicks on context menu item
+    chrome.contextMenus.onClicked.addListener((info) => {
+      //* BUG-FIX: multiple windows led to creation of duplicate tabs equal to number of windows
+      let doesntHaveFocus = document.querySelector("#browser.isblurred");
+      if (doesntHaveFocus) return;
+
+      // Match id used in context menu item creation
+      if (info.menuItemId === "parent-tab") {
+        goToParent();
       }
     });
+
+    // whenever a new tab is created, add its parent tab's url to storage
+    chrome.tabs.onCreated.addListener(saveParentUrl);
 
     // remove storage values when a tab is closed
     chrome.tabs.onRemoved.addListener((tabId) => {
@@ -77,14 +100,7 @@
     // enable/disable the context menu item as applicable
     chrome.tabs.onActivated.addListener((activeInfo) => {
       let tabId = activeInfo.tabId;
-      chrome.storage.local.get(tabId.toString(), (storageItems) => {
-        let savedUrl = storageItems[tabId.toString()];
-        if (!savedUrl || savedUrl === "") {
-          chrome.contextMenus.update("parent-tab", { enabled: false });
-        } else {
-          chrome.contextMenus.update("parent-tab", { enabled: true });
-        }
-      });
+      enableDisableContextMenu(tabId);
     });
   }
 
